66641c3075408cd2839686f8d9d42dc00ab541a4
theashpandey <ashish9145826@gmail.com>
2026-05-03 21:28:13 +0530

    rfr

--- src/main/java/com/javadrill/JavaDrillApplication.java
+++ src/main/java/com/javadrill/JavaDrillApplication.java
@@ -3,7 +3,8 @@ package com.javadrill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class JavaDrillApplication {
